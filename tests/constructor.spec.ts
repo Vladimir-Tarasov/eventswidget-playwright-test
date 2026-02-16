b928ed9e3c61d6371c3a894c340cb7d2dd425365
Vladimir-Tarasov <vtarasov.sk@gmail.com>
2026-02-16 17:49:53 +0100

    test for country section

--- tests/constructor.spec.ts
+++ tests/constructor.spec.ts
@@ -1,3 +1,4 @@
+import { type Page } from '@playwright/test';
 import { test, expect } from './fixtures';
 
 test.describe('Constructor section tests', () => {
@@ -9,6 +10,47 @@ test.describe('Constructor section tests', () => {
   const typeFirstOptionCheckboxSelector = `${typeCheckselectSelector} input[name="type"][value]:not([value=""])`;
   const typeFirstOptionLabelSelector = `${typeCheckselectSelector} label:has(input[name="type"][value]:not([value=""]))`;
   const typeClearSelector = '.checkselect-clear[data-name="type"]';
+  const countryCheckselectSelector = '.checkselect:has(input[name="country"])';
+  const countryDropdownControlSelector = `${countryCheckselectSelector} .checkselect-control`;
+  const countryOptionCheckboxSelector = `${countryCheckselectSelector} input[name="country"]`;
+  const countryOptionLabelSelector = `${countryCheckselectSelector} label:has(input[name="country"])`;
+  const countryClearSelector = '.checkselect-clear[data-name="country"]';
+
+  const verifyDropdownSelectAndClear = async (
+    page: Page,
+    {
+      dropdownControlSelector,
+      optionLabelSelector,
+      optionCheckboxSelector,
+      clearSelector,
+      optionIndex,
+      entityName,
+    }: {
+      dropdownControlSelector: string;
+      optionLabelSelector: string;
+      optionCheckboxSelector: string;
+      clearSelector: string;
+      optionIndex: number;
+      entityName: string;
+    },
+  ) => {
+    const dropdownControl = page.locator(dropdownControlSelector).first();
+    await expect(dropdownControl, `${entityName} dropdown control should be visible`).toBeVisible();
+    await dropdownControl.click();
+
+    const optionLabel = page.locator(optionLabelSelector).nth(optionIndex);
+    const optionCheckbox = page.locator(optionCheckboxSelector).nth(optionIndex);
+
+    await expect(optionLabel, `${entityName} option label should be visible`).toBeVisible();
+    await optionLabel.click();
+    await expect(optionCheckbox, `${entityName} checkbox should be selected`).toBeChecked();
+
+    const clearControl = page.locator(clearSelector).first();
+    await expect(clearControl, `${entityName} clear control should be visible`).toBeVisible();
+    await clearControl.click();
+
+    await expect(optionCheckbox, `${entityName} checkbox should be unselected after clear`).not.toBeChecked();
+  };
 
   test.beforeEach(async ({ page }) => {
     const response = await page.goto(widgetUrl, { waitUntil: 'domcontentloaded' });
@@ -40,21 +82,24 @@ test.describe('Constructor section tests', () => {
   });
 
   test('dropdown checkbox can be selected and cleared', async ({ page }) => {
-    const typeDropdownControl = page.locator(typeDropdownControlSelector).first();
-    await expect(typeDropdownControl, 'Type dropdown control should be visible').toBeVisible();
-    await typeDropdownControl.click();
+    await verifyDropdownSelectAndClear(page, {
+      dropdownControlSelector: typeDropdownControlSelector,
+      optionLabelSelector: typeFirstOptionLabelSelector,
+      optionCheckboxSelector: typeFirstOptionCheckboxSelector,
+      clearSelector: typeClearSelector,
+      optionIndex: 1,
+      entityName: 'Type',
+    });
+  });
 
-    const randomTypeCheckbox = page.locator(typeFirstOptionCheckboxSelector).nth(1);
-    const randomTypeOptionLabel = page.locator(typeFirstOptionLabelSelector).nth(1);
-
-    await expect(randomTypeOptionLabel, 'Random type option label should be visible').toBeVisible();
-    await randomTypeOptionLabel.click();
-    await expect(randomTypeCheckbox, 'Random type checkbox should be selected').toBeChecked();
-
-    const typeClearControl = page.locator(typeClearSelector).first();
-    await expect(typeClearControl, 'Type clear control should be visible').toBeVisible();
-    await typeClearControl.click();
-
-    await expect(randomTypeCheckbox, 'Random type checkbox should be unselected after clear').not.toBeChecked();
+  test('country checkbox can be selected and cleared', async ({ page }) => {
+    await verifyDropdownSelectAndClear(page, {
+      dropdownControlSelector: countryDropdownControlSelector,
+      optionLabelSelector: countryOptionLabelSelector,
+      optionCheckboxSelector: countryOptionCheckboxSelector,
+      clearSelector: countryClearSelector,
+      optionIndex: 0,
+      entityName: 'Country',
+    });
   });
 });
