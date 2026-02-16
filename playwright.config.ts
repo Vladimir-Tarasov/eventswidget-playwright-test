3048a4d94d6515b9b86e3584c15c0e905ad52c2e
Vladimir-Tarasov <vtarasov.sk@gmail.com>
2026-02-16 14:30:23 +0100

    fix playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const isCI = Boolean((globalThis as any).process?.env?.CI);
+
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: isCI,
+  retries: isCI ? 2 : 0,
+  workers: isCI ? 1 : undefined,
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: 'https://dev.3snet.info/eventswidget/',
